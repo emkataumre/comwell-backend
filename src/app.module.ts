62889fdae42d4f12030f1bb9450469e4c7595974
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2023-11-15 10:15:34 +0100

    authentication setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/comwell'),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: 'wPHM6OKJw1VDOqqeDhz7yHDpZcqYTxKpPRgYwOD39PYj1Ie3n5Zn7polO09sVM4',
+};
